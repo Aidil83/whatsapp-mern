0c7c4c422d32cf32c9de8bbd783ae196221e17b1
Aidil <aidilrozi83@gmail.com>
2021-05-16 17:11:24 -0500

    Removed unnecassary code and created component folder.

---
+++ client/pages/login.tsx
@@ -0,0 +1,15 @@
+const login = () => {
+  return (
+    <h1
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      login page
+    </h1>
+  );
+};
+
+export default login;
