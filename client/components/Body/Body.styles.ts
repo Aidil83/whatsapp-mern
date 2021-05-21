ee65d3ddc3761b65f9dfb358ff89352a1f2e679c
Aidil <aidilrozi83@gmail.com>
2021-05-21 15:58:20 -0500

    Removed background repeat-y.

--- client/components/Body/Body.styles.ts
+++ client/components/Body/Body.styles.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const BodyContainer = styled.img`
-  background-repeat: repeat-y;
   background-image: url("/static/images/ws-wallpaper.png");
   height: 100%;
   width: 100%;
