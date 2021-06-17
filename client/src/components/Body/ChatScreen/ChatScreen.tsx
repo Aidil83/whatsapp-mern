eef079d40ec3ff29f156d0690307328bc69e33d1
Aidil <aidilrozi83@gmail.com>
2021-06-16 19:12:21 -0500

    Used FlipMove to animate messages.

--- client/src/components/Body/ChatScreen/ChatScreen.tsx
+++ client/src/components/Body/ChatScreen/ChatScreen.tsx
@@ -1,33 +1,20 @@
-import styled from "styled-components";
 import { BodyWallpaper } from "../Body.styles";
 import Message from "../Message/Message";
 import { useSelector } from "react-redux";
 import { messagesSelector } from "../../../redux/slices/messages.slice";
+import FlipMove from "react-flip-move";
 
 const ChatScreen = () => {
   const messages = useSelector(messagesSelector);
   return (
     <BodyWallpaper>
-      {/* <LeftChat> */}
-      {messages.map((message, idx) => {
-        return <Message key={idx} message={message} />;
-      })}
-      {/* </LeftChat> */}
+      <FlipMove duration={250} easing="ease-out">
+        {messages.map((message) => (
+          <Message message={message} />
+        ))}
+      </FlipMove>
     </BodyWallpaper>
   );
 };
-
-const LeftChat = styled.div`
-  margin-right: auto;
-  width: 200px;
-  height: 50px;
-  border-radius: 8px;
-`;
-const RightChat = styled.div`
-  margin-left: auto;
-  width: 200px;
-  height: 50px;
-  border-radius: 10px;
-`;
 
 export default ChatScreen;
--- client/src/components/Body/Message/Message.tsx
+++ client/src/components/Body/Message/Message.tsx
@@ -1,14 +1,16 @@
+import { forwardRef } from "react";
 import styled from "styled-components";
 import { Typography, CardContent, Card } from "@material-ui/core";
 import { IMessages } from "../../../redux/slices/messages.slice";
 import { useSelector } from "react-redux";
 import { usernameSelector } from "../../../redux/slices/username.slice";
 
-const Message = ({ message }: { message: IMessages }) => {
+// FC do not have a ref, which is needed by react-flip-move to work. Hence, forwardRef is needed.
+const Message = forwardRef(({ message }: { message: IMessages }, ref) => {
   const username = useSelector(usernameSelector);
   const isUser = username === message.username;
   return (
-    <StyledCard isUser={isUser}>
+    <StyledCard isUser={isUser} ref={ref}>
       <CardContent>
         <Typography color="primary" variant="h6" component="h2">
           {message.text || ""}
@@ -16,7 +18,7 @@ const Message = ({ message }: { message: IMessages }) => {
       </CardContent>
     </StyledCard>
   );
-};
+});
 
 const StyledCard = styled(Card)`
   width: fit-content;
