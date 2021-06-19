66dfbc3573da80b2b364e86ea47a9bfc7258ea0e
Aidil <aidilrozi83@gmail.com>
2021-06-19 18:42:50 -0500

    Setting up mongodb.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,26 @@
-import express from 'express';
-const app = express();
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
 
-app.listen('3001', () => {
-  console.log("Server Running on port 3001!");
-})
+// App Config
+const app = express();
+const port = process.env.PORT || 3001;
+const connection_url =
+  "mongodb+srv://admin:CoRrtKEm0AY60fbn@cluster0.cqopk.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+
+// Middlewares
+app.use(express.json());
+
+/* DB config */
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+});
+
+// API Endpoints
+app.get("/", (req, res) => {
+  return res.status(200).send("HELLO EVERYONE");
+});
+// Listener
+app.listen(port, () => {
+  console.log(`Server Running on port ${port}!`);
+});
