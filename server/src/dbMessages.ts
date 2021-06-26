9c77f2a3fa34ee2b052a991ab2a013a90c669b4b
Aidil <aidilrozi83@gmail.com>
2021-06-25 22:32:11 -0500

    Added security dependencies.

--- server/src/dbMessages.ts
+++ server/src/dbMessages.ts
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
-// const whatsappSchema = mongoose.Schema({
-//   message: String,
-//   name: String,
-//   timestamp: String,
-// })
+export interface IUser extends mongoose.Document {
+  message: string;
+  name: string;
+  timestamps: string;
+  received: boolean;
+}
+
+const whatsappSchema = new mongoose.Schema<IUser>({
+  message: { type: String, required: true },
+  name: { type: String, required: true },
+  timestamps: { type: String },
+  received: { type: Boolean, required: true },
+});
+
+const Messages = mongoose.model<IUser>("messageContent", whatsappSchema);
+export default Messages;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,53 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import Cors from "cors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import Messages, { IUser } from "./dbMessages";
 
 // App Config
 const app = express();
-const port = process.env.PORT || 3001;
-const connection_url =
+const port = process.env.PORT || 9000;
+const uri =
   "mongodb+srv://admin:CoRrtKEm0AY60fbn@cluster0.cqopk.mongodb.net/whatsappdb?retryWrites=true&w=majority";
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
+app.use(morgan("common"));
+app.use(helmet());
 
 /* DB config */
-mongoose.connect(connection_url, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// API routes
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
 });
 
 // API Endpoints
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   return res.status(200).send("HELLO EVERYONE");
+});
+app.post("/messages/new", (req: Request, res: Response) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err: Error, data: IUser) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
 });
 // Listener
 app.listen(port, () => {
