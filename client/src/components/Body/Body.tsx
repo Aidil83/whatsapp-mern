804b761ce2afc6a43597f2d72805644eb558255f
Aidil <aidilrozi83@gmail.com>
2021-08-21 13:56:00 -0500

    Added the scroll to bottom functionality. But it doesn't work on first render. Must change default value to something.

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useRef } from "react";
 import { Footer, Header } from "..";
 import { BodyContainer } from "./Body.styles";
 import ChatScreen from "./ChatScreen/ChatScreen";
 
 const Body = () => {
+  let bottomRef = useRef(null);
+
+  const scrollToBottom = () => {
+    if (bottomRef && bottomRef.current) {
+      bottomRef.current.scrollIntoView();
+    }
+  };
+
+  useEffect(scrollToBottom);
+
+  // useEffect(() => {
+  //   console.log(bottomRef.current, "body.tsx");
+  // }, []);
   return (
     <>
       <BodyContainer>
         <Header />
-        <ChatScreen />
+        <ChatScreen forwardedRef={bottomRef} />
         <Footer />
       </BodyContainer>
     </>
--- client/src/components/Body/ChatScreen/ChatScreen.tsx
+++ client/src/components/Body/ChatScreen/ChatScreen.tsx
@@ -4,17 +4,26 @@ import FlipMove from "react-flip-move";
 import { useQuery } from "react-query";
 import * as api from "../../../api/wsApi";
 import { IMessages } from "../../../interfaces/types";
+import { useEffect, useRef } from "react";
 
-const ChatScreen = () => {
+const ChatScreen = ({ forwardedRef }: any) => {
   const { data, isLoading } = useQuery("messages", api.getMessagesData);
   if (isLoading) return <h1>Loading...</h1>;
+
+  // useEffect(() => {
+  //   if (messagesRef.current) {
+  //     scrollToBottom();
+  //   }
+  // }, [messagesRef]);
+
   return (
     <BodyWallpaper>
       <FlipMove duration={250} easing="ease-out">
-        {data.map((messageData: IMessages, idx: number) => (
-          <Message key={idx} messageData={messageData} />
+        {data.map((messageData: IMessages) => (
+          <Message key={messageData._id} messageData={messageData} />
         ))}
       </FlipMove>
+      <div ref={forwardedRef} />
     </BodyWallpaper>
   );
 };
