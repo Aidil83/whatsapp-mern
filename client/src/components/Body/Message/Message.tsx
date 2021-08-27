38cdc7a83c29e446745af0504094d548593ef8e5
Aidil <aidilrozi83@gmail.com>
2021-08-27 15:16:52 -0500

    Working on styling and adding the message descriptions.

--- client/src/components/Body/Message/Message.tsx
+++ client/src/components/Body/Message/Message.tsx
@@ -18,9 +18,23 @@ const Message = forwardRef(
         {isChatName && (
           <StyledCard user={isUser.toString()} ref={ref}>
             <CardContent>
+              <MessageDesc>{messageData.name}</MessageDesc>
               <Typography color="primary" variant="h6" component="h2">
-                {messageData.message}
+                <em style={{ fontWeight: "normal" }}>{messageData.message}</em>
               </Typography>
+              <h5
+                style={{
+                  width: "fit-content",
+                  fontSize: ".7rem",
+                  marginLeft: "auto",
+                  color: "gray",
+                  position: "absolute",
+                  right: 5,
+                  bottom: 2,
+                }}
+              >
+                timestamp
+              </h5>
             </CardContent>
           </StyledCard>
         )}
@@ -32,12 +46,31 @@ const Message = forwardRef(
 const StyledCard = styled(Card)`
   width: fit-content;
   margin: 0.25em;
+  padding: 0.3em;
   ${({ user }: { user: string }) => user == "true" && "margin-left: auto"};
   background-color: ${({ user }: { user: string }) =>
     user == "true" ? "#DCF8C6 !important" : "#fff !important"};
+  &.MuiPaper-rounded {
+    border-radius: 12px;
+  }
   & .MuiTypography-colorPrimary {
     color: ${({ theme }) => theme.primary};
   }
+  & .MuiCardContent-root {
+    position: relative;
+    padding: 0.2em 0.3em;
+    padding-right: 4em;
+    min-width: 4em;
+  }
+  & .MuiCardContent-root:last-child {
+    padding-bottom: 0;
+  }
+`;
+
+const MessageDesc = styled.span`
+  font-size: 0.8em;
+  font-weight: bold;
+  color: blue;
 `;
 
 export default Message;
