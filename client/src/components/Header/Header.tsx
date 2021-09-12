b85ba9756d84b2a83deb9aba2d89930fb6ac5102
Aidil <aidilrozi83@gmail.com>
2021-09-12 15:53:03 -0500

    Added the input searchbox.

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import OutsideClickHandler from "react-outside-click-handler";
 import { useSelector } from "react-redux";
 import { clickChatSelector } from "../../redux/slices/clickChat.slice";
 import { IChip } from "../CreateGroup/CreateGroup";
@@ -10,19 +11,26 @@ import {
   Container,
   DescriptionName,
   NameWrapper,
-  TitleName,
-  StyledSearchIcon,
   StyledMoreVertIcon,
+  StyledSearchIcon,
+  TitleName,
 } from "./Header.styles";
-import OutsideClickHandler from "react-outside-click-handler";
+import Searchbox from "./Searchbox/Searchbox";
 
 const Header = () => {
   const { roomName, image, members } = useSelector(clickChatSelector);
   const [isOpen, setIsOpen] = useState(false);
+  const [isSearchbox, setIsSearchbox] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
+  let searchboxRef = useRef(null);
+
   const handleClickProfile = () => {
     setIsDisabled(false);
     setIsOpen(!isOpen);
+  };
+
+  const handleClickSearchbox = () => {
+    setIsSearchbox(true);
   };
 
   return (
@@ -57,9 +65,14 @@ const Header = () => {
             )}
         </DescriptionName>
       </NameWrapper>
-      <IconButton>
-        <StyledSearchIcon />
-      </IconButton>
+      {!isSearchbox && (
+        <IconButton onClick={handleClickSearchbox}>
+          <StyledSearchIcon />
+        </IconButton>
+      )}
+      <OutsideClickHandler onOutsideClick={() => setIsSearchbox(false)}>
+        {isSearchbox && <Searchbox searchboxRef={searchboxRef} />}
+      </OutsideClickHandler>
       <IconButton>
         <StyledMoreVertIcon />
       </IconButton>
---
+++ client/src/components/Header/Searchbox/Searchbox.tsx
@@ -0,0 +1,93 @@
+import React, { forwardRef, useRef } from "react";
+import Toolbar from "@material-ui/core/Toolbar";
+import InputBase from "@material-ui/core/InputBase";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import { PhoneForwardedRounded } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block",
+      },
+    },
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: "rgba(218, 218, 218, 0.85)",
+      "&:hover": {
+        backgroundColor: "#e2e2e2",
+      },
+      marginLeft: 0,
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(1),
+        width: "auto",
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    inputRoot: {
+      color: "inherit",
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "20ch",
+      },
+    },
+  })
+);
+
+interface ISB {
+  searchboxRef: React.MutableRefObject<HTMLInputElement | null>;
+}
+
+const Searchbox = ({ searchboxRef }: ISB) => {
+  const classes = useStyles();
+
+  React.useEffect(() => {
+    searchboxRef?.current?.focus();
+  }, []);
+
+  return (
+    <Toolbar>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          inputRef={searchboxRef}
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ "aria-label": "search" }}
+        />
+      </div>
+    </Toolbar>
+  );
+};
+
+export default Searchbox;
