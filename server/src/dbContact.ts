443b96cc2cced05bc18431d63d82ce6ee5f040a7
Aidil <aidilrozi83@gmail.com>
2021-10-17 11:12:40 -0500

    Added more field in the dbContact schema.

--- server/src/dbContact.ts
+++ server/src/dbContact.ts
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 
 export interface IChip extends mongoose.Document {
-  id: string;
   name: string;
   image?: string;
-  nameColor: string;
+  about?: string;
+  email?: string;
+  phone?: string;
 }
 
 const ContactSchema = new mongoose.Schema<IChip>({
-  id: { type: Number, required: true },
   name: { type: String, required: true },
   image: { type: String, required: false },
-  nameColor: { type: String, required: true },
+  about: { type: String, required: false },
+  email: { type: String, required: false },
+  phone: { type: String, required: false },
 });
 
-const Chip = mongoose.model<IChip>("contact", ContactSchema);
-export default Chip;
+const Contact = mongoose.model<IChip>("contact", ContactSchema);
+export default Contact;
